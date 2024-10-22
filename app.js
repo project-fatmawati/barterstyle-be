1021d2958bde41465de980aa5b436f6a676098f6
Rika Kamila <rikakamila1717@gmail.com>
2024-10-23 02:05:38 +0700

    Connect DB

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.json({
-      message: "selamat datang di express",
-    });
-  });
+const db = require("./db");
+
+db.then(() => {
+  console.log("berhasil connect ke db");
+}).catch(() => {
+  console.log("gagal konek ke db");
+});
 
 app.listen(PORT, () => {
     console.log("server runnning on port " + PORT);
