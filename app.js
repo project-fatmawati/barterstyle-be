43822b4349774f3b0d6617b30d26709e744888c1
Rika Kamila <rikakamila1717@gmail.com>
2024-10-25 22:27:35 +0700

    Complete Schema model like n member

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const allRoutes = require("./routes");
+const allRoutes = require("./routes");
 const db = require("./db");
 
 db.then(() => {
-  console.log("berhasil connect ke db");
-}).catch(() => {
-  console.log("gagal konek ke db");
+  console.log("connection database success");
+}).catch((err) => {
+  console.log("connection database failed", err);
+  process.exit(1);
 });
 
 app.use(express.json());
-// app.use(allRoutes);
+app.use(allRoutes);
 
 app.listen(PORT, () => {
     console.log("server runnning on port " + PORT);
