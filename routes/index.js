dc8d2f699e1632540ddf138fedb0ff4f237ed4b8
Rika Kamila <rikakamila1717@gmail.com>
2024-10-25 23:19:01 +0700

    complete routes n controllers like n member

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const route = express.Router();
-
 const authRoute = require("./auth-route");
+const likeRoute = require("./like-route");
+const memberRoute = require("./member-route")
+
 const { validateToken } = require("../middleware/auth");
 
 route.use((req, res, next) => {
@@ -16,5 +18,7 @@ route.use((req, res, next) => {
   });
   
   route.use("/auth", authRoute);
+  route.use("/likes", likeRoute);
+  route.use("/member", memberRoute);
 
   module.exports = route;
