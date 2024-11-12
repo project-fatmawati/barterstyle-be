c514124bb1ff7f7ed8f3e6b7f51ecc2948ba42f5
Rika Kamila <rikakamila1717@gmail.com>
2024-11-12 18:58:38 +0700

    clear origin

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5174' }));
+app.use(cors());
 app.use(express.json());
 
 // Koneksi ke MongoDB dengan async/await untuk menangani error
